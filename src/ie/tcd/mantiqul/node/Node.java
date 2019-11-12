efda443309d9aff9cbe8f421a36f9e279807ff9c
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2019-11-12 16:19:28 +0000

    Initial commit
    Add skeleton files

---
+++ src/ie/tcd/mantiqul/node/Node.java
@@ -0,0 +1,102 @@
+package ie.tcd.mantiqul.node;
+
+import ie.tcd.mantiqul.packet.PacketContent;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+
+public abstract class Node {
+  public static final int PACKETSIZE = 65536;
+
+  protected DatagramSocket socket;
+  protected Listener listener;
+  protected CountDownLatch latch;
+
+  Node(int recPort) throws SocketException {
+    latch = new CountDownLatch(1);
+    listener = new SimpleListener();
+    listener.setDaemon(true);
+    listener.start();
+    socket = new DatagramSocket(recPort);
+    listener.go();
+  }
+
+  public abstract void onReceipt(DatagramPacket packet);
+
+  public void send(PacketContent packetContent, InetSocketAddress dstAddress) {
+    send(packetContent, dstAddress.getAddress(), dstAddress.getPort());
+  }
+
+  public void send(PacketContent packetContent, InetAddress dstAddress, int port) {
+    try {
+      DatagramPacket packet = packetContent.toDatagramPacket();
+      setDestination(packet, dstAddress, port);
+      socket.send(packet);
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void setDestination(DatagramPacket packet, InetAddress dstAddress, int port) {
+    packet.setAddress(dstAddress);
+    packet.setPort(port);
+  }
+
+  abstract class Listener extends Thread {
+    /*
+     *  Telling the listener that the socket has been initialized
+     */
+    public void go() {
+      latch.countDown();
+    }
+  }
+
+  /**
+   * SimpleListener thread
+   *
+   * <p>Listens for incoming packets on a datagram socket and informs registered receivers about
+   * incoming packets.
+   */
+  class SimpleListener extends Listener {
+
+    /*
+     * Listen for incoming packets and inform receivers
+     */
+    @Override
+    public void run() {
+      try {
+        latch.await();
+        // Endless loop: attempt to receive packet, notify receivers, etc
+        while (true) {
+          DatagramPacket packet = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+          socket.receive(packet);
+          onReceipt(packet);
+        }
+      } catch (Exception e) {
+        if (!(e instanceof SocketException)) e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    try {
+      String ipAddress = String.format("IP Address : %s\n", InetAddress.getLocalHost().toString());
+      String port = String.format("Port : %s\n", socket.getLocalPort());
+      char[] line = new char[ipAddress.length() * 2];
+      Arrays.fill(line, '-');
+      String lineString = new String(line);
+      return lineString + "\n" + ipAddress + port + lineString;
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
---
+++ src/ie/tcd/mantiqul/packet/PacketContent.java
@@ -0,0 +1,130 @@
+package ie.tcd.mantiqul.packet;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * The class is the basis for packet contents of various types.
+ */
+public abstract class PacketContent {
+
+  public static final int ACK_PACKET = 10;
+  public static final int STRING_PACKET = 101;
+  public static final int WORKER_INFO = 103;
+  public static final int AVAILABILITY = 104;
+  public static final int WORK_INFO = 105;
+  public static final int WORK_RESULT = 106;
+  public static final int WORK_CANCEL = 107;
+
+  public int type = 0;
+  public int size;
+
+  /**
+   * Constructs an object out of a datagram packet.
+   *
+   * @param packet Packet to analyse.
+   */
+  public static PacketContent fromDatagramPacket(DatagramPacket packet) {
+    PacketContent content = null;
+
+    try {
+      int type;
+
+      byte[] data;
+      ByteArrayInputStream bin;
+      ObjectInputStream oin;
+
+      data = packet.getData(); // use packet content as seed for stream
+      bin = new ByteArrayInputStream(data);
+      oin = new ObjectInputStream(bin);
+
+      type = oin.readInt(); // read type from beginning of packet
+
+      switch (type) { // depending on type create content object
+      }
+      if (content != null)
+        content.size = data.length;
+      oin.close();
+      bin.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return content;
+  }
+
+  /**
+   * This method is used to transform content into an output stream.
+   *
+   * @param out Stream to write the content for the packet to.
+   */
+  protected abstract void toObjectOutputStream(ObjectOutputStream out);
+
+  /**
+   * Returns the content of the object as DatagramPacket.
+   *
+   * @return Returns the content of the object as DatagramPacket.
+   */
+  public DatagramPacket toDatagramPacket() {
+    DatagramPacket packet = null;
+    byte[] data = toByteArray();
+    packet = new DatagramPacket(data, data.length); // create packet from byte array
+    size = data.length;
+
+    return packet;
+  }
+
+  /**
+   * Returns the content of the packet as String.
+   *
+   * @return Returns the content of the packet as String.
+   */
+  public abstract String toString();
+
+  /**
+   * Returns the type of the packet.
+   *
+   * @return Returns the type of the packet.
+   */
+  public int getType() {
+    return type;
+  }
+
+  /**
+   * Returns the size of the packet.
+   *
+   * @return Returns the size of the packet.
+   */
+  public int getSize() {
+    return size;
+  }
+
+  public byte[] toByteArray() {
+    try {
+      ByteArrayOutputStream bout;
+      ObjectOutputStream oout;
+      byte[] data;
+
+      bout = new ByteArrayOutputStream();
+      oout = new ObjectOutputStream(bout);
+
+      oout.writeInt(type); // write type to stream
+      toObjectOutputStream(oout); // write content to stream depending on type
+
+      oout.flush();
+      data = bout.toByteArray(); // convert content to byte array
+
+      oout.close();
+      bout.close();
+      return data;
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
