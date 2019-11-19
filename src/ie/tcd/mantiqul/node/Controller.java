ccf784d3ca8e32bbd014b1384c0a7c7821685b1f
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2019-11-19 03:31:52 +0000

    Implement hello packet

--- src/ie/tcd/mantiqul/node/Controller.java
+++ src/ie/tcd/mantiqul/node/Controller.java
@@ -9,6 +9,7 @@ import java.net.SocketException;
 public class Controller extends Node {
 
   Terminal terminal;
+  double versionNumber = 1.0;
 
   Controller(int listeningPort) throws SocketException {
     super(listeningPort);
@@ -25,7 +26,10 @@ public class Controller extends Node {
     PacketContent packetContent = PacketContent.fromDatagramPacket(packet);
     switch (packetContent.type) {
       case PacketContent.HELLO_PACKET:
-        send(new HelloPacketContent(), packet.getAddress(), packet.getPort());
+        HelloPacketContent helloPacketContent = (HelloPacketContent) packetContent;
+        double receivedVersionNumber = helloPacketContent.getVersionNumber();
+        if (receivedVersionNumber < versionNumber) versionNumber = receivedVersionNumber;
+        send(new HelloPacketContent(versionNumber), packet.getAddress(), packet.getPort());
         break;
     }
   }
