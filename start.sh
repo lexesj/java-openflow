3a15a2d8385614207cf0c0904fe7c5c2aa04c2df
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2019-11-20 02:07:07 +0000

    Fix negative node id bug

--- start.sh
+++ start.sh
@@ -14,16 +14,20 @@ sudo docker network create --subnet 172.0.20.0/16 $NETWORK_NAME
 # allow controller to initialise
 sleep 1
 
-NUM_PORTS=$(($1 - 1))
+if [ $# -ne 0 ]; then
+  NUM_PORTS=$(($1 - 1))
+else
+  NUM_PORTS=0;
+fi
 
 for i in `seq 0 $NUM_PORTS`;
 do
   if [ $i -ne $NUM_PORTS ]; then
-    ./switch.sh $i switch$(($i + 1)) &
+    ./switch.sh $i switch$(($i + 1)).telecomms &
   else
-    ./switch.sh $i endpoint2 &
+    ./switch.sh $i endpoint1.telecomms &
   fi
 done
 
-./endpoint.sh 0 switch0 &
-./endpoint.sh 1 switch$NUM_PORTS &
+./endpoint.sh 0 switch0.telecomms &
+./endpoint.sh 1 switch$NUM_PORTS.telecomms &
